c1ae6752201504753ca572655f59d3d9c7f51c0f
Andy Edwards <andyedwards@outlook.com>
2025-02-17 22:07:47 -0500

    Added media query for small screen

--- puppyBowlWorkshop/src/components/AllPlayers.jsx
+++ puppyBowlWorkshop/src/components/AllPlayers.jsx
@@ -19,7 +19,7 @@ const AllPlayers = ({ searchQuery }) => {
       } catch (error) {
         console.error("Failed to fetch players:", error);
       }
-    }
+    };
 
     getPlayers();
   }, []);
@@ -28,9 +28,7 @@ const AllPlayers = ({ searchQuery }) => {
     if (!searchQuery) {
       setFilteredPlayers(players);
     } else {
-      setFilteredPlayers(players.filter((player) =>
-        player.name.toLowerCase().includes(searchQuery.toLowerCase())
-      ));
+      setFilteredPlayers(players.filter((player) => player.name.toLowerCase().includes(searchQuery.toLowerCase())));
     }
   }, [searchQuery, players]);
 
@@ -41,40 +39,39 @@ const AllPlayers = ({ searchQuery }) => {
     try {
       const success = await deletePlayer(playerId);
       if (success) {
-        const updatedPlayers = players.filter(player => player.id !== playerId);
+        const updatedPlayers = players.filter((player) => player.id !== playerId);
         setPlayers(updatedPlayers);
         setFilteredPlayers(updatedPlayers);
 
         alert("Player deleted successfully!");
       } else {
-      alert("Failed to delete player. Please try again.");
-      } 
+        alert("Failed to delete player. Please try again.");
+      }
     } catch (error) {
       console.error("Error deleting player:", error);
     }
-    };
-
+  };
 
   return (
     <div>
-      <h1>Puppy Players</h1>
-      <div className="players-container">
+      <h1 className='pup-players-heading'>Puppy Players</h1>
+      <div className='players-container'>
         {filteredPlayers.length > 0 ? (
           filteredPlayers.map((player) => (
-            <div key={player.id} className="player-card">
+            <div key={player.id} className='player-card'>
               <img src={player.imageUrl} alt={`Image of ${player.name}`} />
               <h4>{player.name}</h4>
               <p>{player.breed}</p>
-              <button type="button" onClick={() => navigate(`/players/${player.id}`)}>
+              <button type='button' onClick={() => navigate(`/players/${player.id}`)}>
                 See Player Details
               </button>
-              <button type="button" onClick={() => handleDelete(player.id)}>
+              <button type='button' onClick={() => handleDelete(player.id)}>
                 Delete Player
               </button>
             </div>
           ))
         ) : (
-            <p>Loading players...</p>
+          <p>Loading players...</p>
         )}
       </div>
     </div>
